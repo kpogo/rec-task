577c966cd9cf317bafeea95d99be68056047ae15
Krzysztof Pogorzelski <k.pogo92@gmail.com>
2019-10-29 06:56:24 +0000

    Updates

---
+++ precache-manifest.394ea951951c238094f9b65135d22491.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cadd19c17c2326bf222c7cbda9f2f950",
+    "url": "/rec-task/index.html"
+  },
+  {
+    "revision": "f4a65ddd1f30f9a6206a",
+    "url": "/rec-task/static/css/main.dff5f361.chunk.css"
+  },
+  {
+    "revision": "d87bd7dfffc97d0fcac1",
+    "url": "/rec-task/static/js/2.40da3b6f.chunk.js"
+  },
+  {
+    "revision": "f4a65ddd1f30f9a6206a",
+    "url": "/rec-task/static/js/main.e295f54a.chunk.js"
+  },
+  {
+    "revision": "c32f96f37e333be74523",
+    "url": "/rec-task/static/js/runtime-main.f529be79.js"
+  }
+]);
